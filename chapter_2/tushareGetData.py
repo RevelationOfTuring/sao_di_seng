d5357cd4ccdbfceefa47e2e7ab64a85992263159
MichaelWang <michael_wangguanyi@163.com>
2023-01-13 16:10:31 +0800

    upd

---
+++ chapter_2/tushareGetData.py
@@ -0,0 +1,15 @@
+import tushare as ts
+
+# 设置token，要用你自己的token号
+# https://tushare.pro/user/token
+ts.set_token('填写自己的token号')
+
+# 行情结果存放到pandas dataframe，按日期降序排
+df = ts.pro_bar(
+    ts_code='600000.SH', adj='hfq', start_date='20000101',
+    end_date='20200710')  # 日线,前复权qfq，后复权hfq
+
+# 颠倒顺序，使得按日期升序排。backtrader要求日期升序
+df.sort_index(inplace=True, ascending=False)
+
+df.to_csv('../data/600000.csv')
